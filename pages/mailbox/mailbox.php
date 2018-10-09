719e545255727c9a3760249f5e86ee545e509dde
pelbe <pelbek22@hotmail.com>
2018-10-09 15:36:23 +0300

    Bekir

--- pages/mailbox/mailbox.php
+++ pages/mailbox/mailbox.php
@@ -4,37 +4,23 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Ana Sayfa | Posta Kutusu</title>
-
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  
   <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
-
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-  
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
- 
   <link rel="stylesheet" href="../../plugins/fullcalendar/fullcalendar.min.css">
   <link rel="stylesheet" href="../../plugins/fullcalendar/fullcalendar.print.css" media="print">
-
   <link rel="stylesheet" href="../../dist/css/AdminLTE.min.css">
-  
   <link rel="stylesheet" href="../../dist/css/skins/_all-skins.min.css">
-  
   <link rel="stylesheet" href="../../plugins/iCheck/flat/blue.css">
-  
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
-
   <header class="main-header">
-  
-    <a href="../../index.html" class="logo">
-
+    <a href="index.html" class="logo">
       <span class="logo-mini"><b>A</b>S</span>
-      
       <span class="logo-lg"><b>Ana</b>SAYFA</span>
     </a>
-  
     <nav class="navbar navbar-static-top">
 
       <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
@@ -42,29 +28,22 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </a>
-
       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
-          
           <li class="dropdown tasks-menu">
             </a>
             <ul class="dropdown-menu">
-              
                 <ul class="menu">
                   <li>
                     <a href="#">
                       <h3>
-                    
-                       
                       </h3>
                       <div class="progress xs">
                         <div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                          
                         </div>
                       </div>
                     </a>
                   </li>
-            
                 </ul>
               </li>
               <li class="footer">
@@ -72,23 +51,19 @@
               </li>
             </ul>
           </li>
-        
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <img src="../../dist/img/avatar5.png" class="user-image" alt="User Image">
               <span class="hidden-xs">Bekir Uyar</span>
             </a>
             <ul class="dropdown-menu">
-      
               <li class="user-header">
                 <img src="../../dist/img/avatar5.png" class="img-circle" alt="User Image">
-
                 <p>
                   Bekir Uyar - Site Admini
                   <small>Bu Tarihten beri (Kasım  2000)</small>
                 </p>
               </li>
-            
               <li class="user-body">
                 <div class="row">
                   <div class="col-xs-4 text-center">
@@ -101,9 +76,7 @@
                     <a href="#">Arkadaşlar</a>
                   </div>
                 </div>
-    
               </li>
-             
               <li class="user-footer">
                 <div class="pull-left">
                   <a href="giris.html" class="btn btn-default btn-flat" >Giriş Yap</a>
@@ -114,7 +87,6 @@
               </li>
             </ul>
           </li>
-     
           <li>
             <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
           </li>
@@ -122,11 +94,8 @@
       </div>
     </nav>
   </header>
-
   <aside class="main-sidebar">
-  
     <section class="sidebar">
-
       <div class="user-panel">
         <div class="pull-left image">
           <img src="../../dist/img/avatar5.png" class="img-circle" alt="User Image">
@@ -136,7 +105,6 @@
           <a href="#"><i class="fa fa-circle text-success"></i> Yönetici</a>
         </div>
       </div>
-      
       <form action="#" method="get" class="sidebar-form">
         <div class="input-group">
           <input type="text" name="q" class="form-control" placeholder="Arama...">
@@ -146,7 +114,6 @@
               </span>
         </div>
       </form>
-     
       <ul class="sidebar-menu">
         <li class="header">Yönlendirici</li>
         <li class="treeview">
@@ -157,8 +124,7 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="../../index.html"><i class="fa fa-circle-o"></i>Ana Sayfa</a></li>
-          
+            <li><a href="index.html"><i class="fa fa-circle-o"></i>Ana Sayfa</a></li>
           </ul>
         </li>
         <li class="treeview">
@@ -171,10 +137,8 @@
           </a>
           <ul class="treeview-menu">
             <li><a href="C:\Users\pelbe\Desktop\hi\pages/layout/uye.html"><i class="fa fa-circle-o"></i>Üyeler</a></li>
-    
           </ul>
         </li>
-    
         <li>
           <a href="../calendar.html">
             <i class="fa fa-calendar"></i> <span>Takvim</span>
@@ -199,24 +163,18 @@
                 </span>
               </a>
             </li>
-            <li><a href="compose.html">Oluştur</a></li>
-            <li><a href="read-mail.html">Oku</a></li>
+            <li><a href="compose.php">Oluştur</a></li>
+            <li><a href="read-mail.php">Oku</a></li>
           </ul>
         </li>
-
         <li class="header">İletişim</li>
-        
         <li><a href="https://www.instagram.com/bekirff/?hl=tr" target="_Blank"><i class="fa fa-circle-o text-red"></i> <span>İnstagram</span></a></li>
         <li><a href="mailto:bekiruyar324@gmail.com" ><i class="fa fa-circle-o text-yellow"></i> <span>Gmail</span></a></li>
         <li><a href="https://www.facebook.com/bekirff" target="_Blank"><i class="fa fa-circle-o text-aqua"></i> <span>Facebook</span></a></li>
       </ul>
     </section>
- 
   </aside>
-
- 
   <div class="content-wrapper">
-  
     <section class="content-header">
       <h1>
         Posta Kutusu
@@ -227,8 +185,6 @@
         <li class="active">Posta Kutusu</li>
       </ol>
     </section>
-
-   
     <section class="content">
       <div class="row">
         <div class="col-md-3">
@@ -254,9 +210,7 @@
                 <li><a href="#"><i class="fa fa-trash-o"></i> Çöp</a></li>
               </ul>
             </div>
-    
           </div>
-         
           <div class="box box-solid">
             <div class="box-header with-border">
               <h3 class="box-title">Ulaşabileceğiniz diğer yollar</h3>
@@ -269,32 +223,24 @@
             <div class="box-body no-padding">
               <ul class="nav nav-pills nav-stacked">
                 <li><a href="https://www.facebook.com/bekirff" target="_Blank"><i class="fa fa-circle-o text-red"></i> İnstagram</a></li>
-          
                 <li><a href="https://www.instagram.com/bekirff/?hl=tr " target="_Blank"><i class="fa fa-circle-o text-light-blue"></i> Facebook</a></li>
               </ul>
             </div>
-  
           </div>
-      
         </div>
-
         <div class="col-md-9">
           <div class="box box-primary">
             <div class="box-header with-border">
               <h3 class="box-title">Gelen Kutusu</h3>
-
               <div class="box-tools pull-right">
                 <div class="has-feedback">
                   <input type="text" class="form-control input-sm" placeholder="Mail Ara">
                   <span class="glyphicon glyphicon-search form-control-feedback"></span>
                 </div>
               </div>
-        
             </div>
-        
             <div class="box-body no-padding">
               <div class="mailbox-controls">
-      
                 <button type="button" class="btn btn-default btn-sm checkbox-toggle"><i class="fa fa-square-o"></i>
                 </button>
                 <div class="btn-group">
@@ -302,7 +248,6 @@
                   <button type="button" class="btn btn-default btn-sm"><i class="fa fa-reply"></i></button>
                   <button type="button" class="btn btn-default btn-sm"><i class="fa fa-share"></i></button>
                 </div>
-                <!-- /.btn-group -->
                 <button type="button" class="btn btn-default btn-sm"><i class="fa fa-refresh"></i></button>
                 <div class="pull-right">
                   1-50/200
@@ -310,9 +255,7 @@
                     <button type="button" class="btn btn-default btn-sm"><i class="fa fa-chevron-left"></i></button>
                     <button type="button" class="btn btn-default btn-sm"><i class="fa fa-chevron-right"></i></button>
                   </div>
-                  <!-- /.btn-group -->
                 </div>
-                <!-- /.pull-right -->
               </div>
               <div class="table-responsive mailbox-messages">
                 <table class="table table-hover table-striped">
@@ -320,7 +263,7 @@
                   <tr>
                     <td><input type="checkbox"></td>
                     <td class="mailbox-star"><a href="#"><i class="fa fa-star text-yellow"></i></a></td>
-                    <td class="mailbox-name"><a href="read-mail.html">Bekir Uyar</a></td>
+                    <td class="mailbox-name"><a href="read-mail.php">Bekir Uyar</a></td>
                     <td class="mailbox-subject"><b>Yönetim 2.0 Konu</b> - Bu soruna bir çözüm bulmaya çalışıyorum...
                     </td>
                     <td class="mailbox-attachment"></td>
@@ -329,7 +272,7 @@
                   <tr>
                     <td><input type="checkbox"></td>
                     <td class="mailbox-star"><a href="#"><i class="fa fa-star-o text-yellow"></i></a></td>
-                    <td class="mailbox-name"><a href="read-mail.html">Bekir Uyar</a></td>
+                    <td class="mailbox-name"><a href="read-mail.php">Bekir Uyar</a></td>
                     <td class="mailbox-subject"><b>Yönetim 2.0 Konu</b> - Bu soruna bir çözüm bulmaya çalışıyorum...
                     </td>
                     <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
@@ -338,7 +281,7 @@
                   <tr>
                     <td><input type="checkbox"></td>
                     <td class="mailbox-star"><a href="#"><i class="fa fa-star-o text-yellow"></i></a></td>
-                    <td class="mailbox-name"><a href="read-mail.html">Bekir Uyar</a></td>
+                    <td class="mailbox-name"><a href="read-mail.php">Bekir Uyar</a></td>
                     <td class="mailbox-subject"><b>Yönetim 2.0 Konu</b> - Bu soruna bir çözüm bulmaya çalışıyorum...
                     </td>
                     <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
@@ -454,14 +397,10 @@
                   </tr>
                   </tbody>
                 </table>
-          
               </div>
- 
             </div>
-        
             <div class="box-footer no-padding">
               <div class="mailbox-controls">
-
                 <button type="button" class="btn btn-default btn-sm checkbox-toggle"><i class="fa fa-square-o"></i>
                 </button>
                 <div class="btn-group">
@@ -469,7 +408,6 @@
                   <button type="button" class="btn btn-default btn-sm"><i class="fa fa-reply"></i></button>
                   <button type="button" class="btn btn-default btn-sm"><i class="fa fa-share"></i></button>
                 </div>
-             
                 <button type="button" class="btn btn-default btn-sm"><i class="fa fa-refresh"></i></button>
                 <div class="pull-right">
                   1-50/200
@@ -477,104 +415,62 @@
                     <button type="button" class="btn btn-default btn-sm"><i class="fa fa-chevron-left"></i></button>
                     <button type="button" class="btn btn-default btn-sm"><i class="fa fa-chevron-right"></i></button>
                   </div>
-   
                 </div>
-
               </div>
             </div>
           </div>
-
         </div>
-
       </div>
-
     </section>
-
   </div>
-
   <footer class="main-footer">
     <div class="pull-right hidden-xs">
-     
     </div>
-   
   </footer>
-
-
   <aside class="control-sidebar control-sidebar-dark">
-
     <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
       <li><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
       <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
     </ul>
     <div class="tab-content">
-    
-            
-          
-            
-      
-        
-
         </form>
       </div>
-
     </div>
   </aside>
-
   <div class="control-sidebar-bg"></div>
 </div>
-
-
-
 <script src="../../plugins/jQuery/jquery-2.2.3.min.js"></script>
-
 <script src="../../bootstrap/js/bootstrap.min.js"></script>
-
 <script src="../../plugins/slimScroll/jquery.slimscroll.min.js"></script>
-
 <script src="../../plugins/fastclick/fastclick.js"></script>
-
 <script src="../../dist/js/app.min.js"></script>
-<!-- iCheck -->
 <script src="../../plugins/iCheck/icheck.min.js"></script>
-<!-- Page Script -->
 <script>
   $(function () {
-    //Enable iCheck plugin for checkboxes
-    //iCheck for checkbox and radio inputs
     $('.mailbox-messages input[type="checkbox"]').iCheck({
       checkboxClass: 'icheckbox_flat-blue',
       radioClass: 'iradio_flat-blue'
     });
-
-    //Enable check and uncheck all functionality
     $(".checkbox-toggle").click(function () {
       var clicks = $(this).data('clicks');
       if (clicks) {
-        //Uncheck all checkboxes
         $(".mailbox-messages input[type='checkbox']").iCheck("uncheck");
         $(".fa", this).removeClass("fa-check-square-o").addClass('fa-square-o');
       } else {
-        //Check all checkboxes
         $(".mailbox-messages input[type='checkbox']").iCheck("check");
         $(".fa", this).removeClass("fa-square-o").addClass('fa-check-square-o');
       }
       $(this).data("clicks", !clicks);
     });
-
-    //Handle starring for glyphicon and font awesome
     $(".mailbox-star").click(function (e) {
       e.preventDefault();
-      //detect type
       var $this = $(this).find("a > i");
       var glyph = $this.hasClass("glyphicon");
       var fa = $this.hasClass("fa");
-
-      //Switch states
       if (glyph) {
         $this.toggleClass("glyphicon-star");
         $this.toggleClass("glyphicon-star-empty");
       }
-
       if (fa) {
         $this.toggleClass("fa-star");
         $this.toggleClass("fa-star-o");
@@ -582,7 +478,6 @@
     });
   });
 </script>
-<!-- AdminLTE for demo purposes -->
 <script src="../../dist/js/demo.js"></script>
 </body>
 </html>
