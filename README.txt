1215ae0a0bf87aca0cc32dbecb82f8253515bc71
pelbe <pelbek22@hotmail.com>
2018-10-09 15:08:20 +0300

    Bekir

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/bekir-uyar/pen/jvjbvY.
+
+ 
---
+++ s.php
@@ -0,0 +1,9 @@
+<?php
+
+
+try{
+    $db = new PDO("mysql:host=localhost;dbname=kisisel;charset=utf8","root","");
+}catch(PDOException $error)
+{
+    die($error->GetMessage());
+}
