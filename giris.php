3ac4d697d28342157e56eca0342464763157545e
pelbe <pelbek22@hotmail.com>
2018-10-10 13:29:58 +0300

    Bekir

--- giris.php
+++ giris.php
@@ -26,7 +26,7 @@ if ($_POST)
     if (empty($kadi) || empty($passaword)) {
         echo 'Kullanıcı Adınızı & Şifrenizi Boş Bırakmayınız...';
     } else {
-        $query = $db->query("SELECT * FROM uye WHERE kadi= '{$id}' AND sifre='{$password}'")->fetch(PDO::FETCH_ASSOC);
+        $query = $db->query("SELECT * FROM uye WHERE kadi='{$kadi}' AND sifre='{$passaword}'")->fetch(PDO::FETCH_ASSOC);
 if ($query){
     print_r($query);
 }
