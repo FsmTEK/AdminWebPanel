886ff021681c0ca668872d4e763c06181c3018fa
pelbe <pelbek22@hotmail.com>
2018-10-10 12:30:04 +0300

    Bekir

--- index.php
+++ index.php
@@ -1,245 +1,34 @@
- <html>
+<!DOCTYPE html>
+<html lang="en" >
+
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Yönetici</title>
+  <meta charset="UTF-8">
+  <title>Bootstrap Snippet: Login Form</title>
+  
+  
+  <link rel='stylesheet' href='http://netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css'>
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-  
-    
-  <link rel="stylesheet" href="faa/style.css">
-  
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
- 
-  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-  
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+      <link rel="stylesheet" href="cssc/style.css">
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-
-  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
- 
-  <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
- 
-  <link rel="stylesheet" href="plugins/iCheck/flat/blue.css">
-
-  <link rel="stylesheet" href="plugins/morris/morris.css">
   
-  <link rel="stylesheet" href="plugins/jvectormap/jquery-jvectormap-1.2.2.css">
-  
-  <link rel="stylesheet" href="plugins/datepicker/datepicker3.css">
- 
-  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
-  
-  <link rel="stylesheet" href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+</head>
 
-    </head>
-    <body class="hold-transition skin-Fsidebar-mini">
+<body>
+
     <div class="wrapper">
-        <header class="main-header">
-            <a href="index.html" class="logo">
-                 <span class="logo-mini"><b>A</b>S</span>
-                 <span class="logo-lg"><b>Ana</b>SAYFA</span>
-             </a>
-         <nav class="navbar navbar-static-top">  
-             </a>
-       <div class="navbar-custom-menu">
-          <ul class="nav navbar-nav">
-           <ul class="dropdown-menu">  
-            <ul class="menu">  
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <div class="pull-left">
-                      </div>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-            <ul class="dropdown-menu">
-                <ul class="menu">
-                </ul>
-            </ul>
-              <li class="dropdown tasks-menu">
-            </a>
-             <ul class="dropdown-menu">
-              <ul class="menu">
-                </ul>
-              </li>
-              <li class="footer">
-              </li>
-            </ul>
-          </li>
-        <li class="dropdown user user-menu">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-               <img src="dist/img/avatar5.png" class="user-image" alt="User Image">
-               <span class="hidden-xs">Bekir Uyar</span>
-          </a>
-               <ul class="dropdown-menu">
-            
-                     <li class="user-header">
-                      <img src="dist/img/avatar5.png" class="img-circle" alt="User Image">
-                <p>
-                  Bekir Uyar - Teknoloji Admini
-                  <small>En Başından beri üye. 2000</small>
-                </p>
-              </li>
-             
-              <li class="user-footer">
-                <div class="pull-left">
-                  <a href="giris.php" class="btn btn-default btn-flat" >Giriş Yap</a>
-                </div>
-                <div class="pull-right">
-                  <a href="register.php" class="btn btn-default btn-flat">Kayıt Ol</a>
-                </div>
-              </li>
-            </ul>
-          </li>
-        
-        </ul>
-      </div>
-    </nav>
-  </header>
- 
-  <aside class="main-sidebar">
-
-    <section class="sidebar">
-     
-      <div class="user-panel">
-        <div class="pull-left image">
-          <img src="dist/img/avatar5.png" class="img-circle" alt="User Image">
-        </div>
-        <div class="pull-left info">
-          <p>Bekir Uyar</p>
-          <a href="#"><i class="fa fa-circle text-success"></i>Yönetici</a>
-        </div>
-      </div>
-    
-      <form action="#" method="get" class="sidebar-form">
-        <div class="input-group">
-          <input type="text" name="q" class="form-control" placeholder="Arama...">
-              <span class="input-group-btn">
-                <button type="submit" name="Arama" id="Arama-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-        </div>
-      </form>
-     
-      <ul class="sidebar-menu">
-        <li class="header">Panel</li>
-        <li class="active treeview">
-          <a href="#">
-            <i class="fa fa-dashboard"></i> <span>Ana Sayfa</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="index.html"><i class="fa fa-circle-o"></i>Ana Sayfa</a></li>
-          </ul>
-        </li>
-        
-        
-        </li>
-<li class="treeview">
-          <a href="#">
-            <i class="fa fa-files-o"></i>
-            <span>Üye Paneli</span>
-            <span class="pull-right-container">
-            
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="pages/layout/uye.html"><i class="fa fa-circle-o"></i>Üyeler</a></li>
-          </ul>
-        </li>
-      
-        <li>
-          <a href="pages/mailbox/mailbox.html">
-            <i class="fa fa-envelope"></i> <span>Gelen Kutusu </span>
-            <span class="pull-right-container">
-              <small class="label pull-right bg-yellow">12</small>
-              <small class="label pull-right bg-green">16</small>
-              <small class="label pull-right bg-red">5</small>
-            </span>
-          </a>
-        </li>
-        <li class="treeview">
-        
-        <li class="header">İletişim </li>
-        <li><a href="https://www.instagram.com/bekirff/?hl=tr" target="_Blank"><i class="fa fa-circle-o text-red"></i> <span>İnstagram</span></a></li>
-        <li><a href="mailto:bekiruyar324@gmail.com" ><i class="fa fa-circle-o text-yellow"></i> <span>Gmail</span></a></li>
-        <li><a href="https://www.facebook.com/bekirff" target="_Blank"><i class="fa fa-circle-o text-aqua"></i> <span>Facebook</span></a></li>
-      </ul>
-    </section>
-  </aside>
-  <div class="content-wrapper">
-    <section class="content-header">
-      <h1>
-        Ana Sayfa
-        <small></small>
-      </h1>
-      
-    </section>
-  <div class="image-container">
+    <form class="form-signin">       
+      <h2 class="form-signin-heading">Please login</h2>
+      <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" />
+      <input type="password" class="form-control" name="password" placeholder="Password" required=""/>      
+      <label class="checkbox">
+        <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Remember me
+      </label>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>   
+    </form>
+  </div>
   
-  </div>
-  <div class="title-container">
-  <h1>Teknoloji tasarımlı<br/> web sitemize <br/> HOŞGELDİNİZ</h1> 
-  </div>
-  <div class="rectangle5"></div>
-  <div class="rectangle4"></div>
-  <div class="rectangle3"></div>
-  <div class="rectangle2"></div>
-  <div class="rectangle1"></div>
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
-
   
 
-    <script  src="jss/index.js"></script>
-
-  <div class="control-sidebar-bg"></div>
-</div>
-
-<script src="plugins/jQuery/jquery-2.2.3.min.js"></script>
-
-<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
-
-<script>
-  $.widget.bridge('uibutton', $.ui.button);
-</script>
-
-<script src="bootstrap/js/bootstrap.min.js"></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-<script src="plugins/morris/morris.min.js"></script>
-
-<script src="plugins/sparkline/jquery.sparkline.min.js"></script>
-
-<script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-
-<script src="plugins/knob/jquery.knob.js"></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
-<script src="plugins/daterangepicker/daterangepicker.js"></script>
-
-<script src="plugins/datepicker/bootstrap-datepicker.js"></script>
-
-<script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-
-<script src="plugins/slimScroll/jquery.slimscroll.min.js"></script>
-
-<script src="plugins/fastclick/fastclick.js"></script>
-
-<script src="dist/js/app.min.js"></script>
-
-<script src="dist/js/pages/dashboard.js"></script>
-
-<script src="dist/js/demo.js"></script>
 </body>
 
 </html>
